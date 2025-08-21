f4870666eeb03c78fd7e7e1940e3ec2091cfcae8
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-21 12:33:17 +0200

    add: Created quiz model which will reference lesson model

---
+++ backend/src/models/Quiz.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IQuestion {
+    questionText: string;
+    questionType: string;
+    options: string[]; // array of all answers
+    correctAnswer: string;
+}
+
+// declare the main interface for the Quiz doc
+export interface IQuiz extends Document {
+  lesson: mongoose.Schema.Types.ObjectId;
+  questions: IQuestion[];
+}
+
+const QuizSchema: Schema = new Schema({
+    // "lesson" field is a reference to the 'Lesson' collection
+    lesson: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lesson",
+        required: true,
+        unique: true, // a lesson should only have one quiz associated with it
+    },
+    questions: [
+      {
+        questionText: {
+          type: String,
+          required: true,
+        },
+        questionType: {
+          type: String,
+          required: true,
+        },
+        options: {
+          type: [String],
+          required: true,
+        },
+        correctAnswer: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+});
+
+export default mongoose.model<IQuiz>("Quiz", QuizSchema);
