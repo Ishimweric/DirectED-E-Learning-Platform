f403528ef9d866dd188ed050256ee2b2b91bbf3b
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-21 17:09:51 +0200

    fix: Fixed quiz model

--- backend/src/models/Quiz.ts
+++ backend/src/models/Quiz.ts
@@ -1,47 +1,60 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-export interface IQuestion {
-    _id: mongoose.Types.ObjectId;
-    questionText: string;
-    questionType: string;
-    options: string[]; // array of all answers
-    correctAnswer: string;
+interface IQuestion {
+  questionText: string;
+  options: string[];
+  correctAnswer: string;
 }
 
-// declare the main interface for the Quiz doc
 export interface IQuiz extends Document {
   lesson: mongoose.Schema.Types.ObjectId;
   questions: IQuestion[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const QuizSchema: Schema = new Schema({
-    // "lesson" field is a reference to the 'Lesson' collection
-    lesson: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Lesson",
-        required: true,
-        unique: true, // a lesson should only have one quiz associated with it
-    },
-    questions: [
-      {
-        questionText: {
-          type: String,
-          required: true,
-        },
-        questionType: {
-          type: String,
-          required: true,
-        },
-        options: {
-          type: [String],
-          required: true,
-        },
-        correctAnswer: {
-          type: String,
-          required: true,
-        },
-      },
-    ],
+const QuestionSchema: Schema = new Schema({
+  questionText: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: [String],
+    required: true,
+  },
+  correctAnswer: {
+    type: String,
+    required: true,
+  },
 });
 
-export default mongoose.model<IQuiz>("Quiz", QuizSchema);
+const QuizSchema: Schema = new Schema({
+  
+  lesson: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Lesson',
+    required: true,
+    unique: true,
+  },
+  
+  questions: {
+    type: [QuestionSchema],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+QuizSchema.pre<IQuiz>('save', function(next) {
+  this.updatedAt = new Date();
+  next();
+});
+
+const Quiz = mongoose.model<IQuiz>('Quiz', QuizSchema);
+export default Quiz;
