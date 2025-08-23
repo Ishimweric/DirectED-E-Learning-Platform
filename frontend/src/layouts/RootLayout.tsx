ca994cdff07e2f665a35c20b1a70342ea45e8e7b
Ephrata <ephrata0327@gmail.com>
2025-08-23 10:13:43 +0300

    add a rootlayout file

---
+++ frontend/src/layouts/RootLayout.tsx
@@ -0,0 +1,35 @@
+import type React from "react"
+import  GeistSans  from "geist/font/sans"
+import { AuthProvider } from "../context/AuthContext"
+import { ThemeProvider } from "../context/ThemeContext"
+import { DashboardLayout } from "../components/layouts/DashboardLayout"
+import "./globals.css"
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <style>{`
+html {
+  font-family: ${GeistSans.style.fontFamily};
+  --font-sans: ${GeistSans.variable};
+  --font-mono: ${GeistSans.variable};
+  
+}
+        `}</style>
+      </head>
+      <body className={`${GeistSans.variable}  antialiased`}>
+        <ThemeProvider>
+          <AuthProvider>
+            <DashboardLayout>{children}</DashboardLayout>
+          </AuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
