0ddfd094a65baea9e4b2e76bb6a41b68149a01f4
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-24 09:00:49 +0200

    fix: Fixed the hero section to be responsive

--- frontend/src/components/Landing/HeroSection.tsx
+++ frontend/src/components/Landing/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="gradient-bg text-white py-20 h-[calc(100vh-75px)] flex w-full">
+    <section className="gradient-bg text-white py-20 h-[calc(100vh-75px)] flex w-full md:flex-col">
       <div className="container mx-auto px-4 w-2/3">
         <div className="max-w-4xl mx-auto text-center">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6">
