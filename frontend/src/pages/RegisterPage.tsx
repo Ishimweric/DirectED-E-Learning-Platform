7a4e5956149717ef0315c96160c6924cd0ca2916
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-25 17:02:03 +0200

    fix: Improved sign up page

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -44,7 +44,7 @@ const RegisterPage: React.FC = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full">
+      <div className="w-full flex">
         {/* Header */}
         <div className="text-center mb-8 flex-1 md:flex hidden  items-center justify-center flex-col px-3 gap-4">
           <div className="flex justify-center mb-4">
